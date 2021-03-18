7b23744a8635ec1fd4c768180914c14d6ebc0c5d
sean <pulpfictionost@gmail.com>
2021-03-18 14:08:53 +0000

    Read Rom contents & print a few bytes in hex

--- src/halboy.c
+++ src/halboy.c
@@ -1,6 +1,21 @@
 #include "halboy.h"
 #include <glib.h>
 
+void readrom()
+{
+    gsize length;
+    gchar *filename = "sml2.gb";
+    gchar *contents; // = malloc(sizeof(gchar) * 0x0080000);
+    g_file_get_contents(filename, &contents, &length, NULL);
+    g_print("file length: %ld\n", length);
+
+    for (size_t i = 0; i < 16; i++)
+    {
+        g_print("%02x ", (guchar)contents[i]);
+    }
+    g_free(contents);
+}
+
 int main(int argc, char *argv[])
 {
     struct registers *r = malloc(sizeof(struct registers));
@@ -10,4 +25,6 @@ int main(int argc, char *argv[])
         g_print("%d\n", r->a);
         g_print("%d\n", r->af);
     }
+    readrom();
+    g_free(r);
 }
